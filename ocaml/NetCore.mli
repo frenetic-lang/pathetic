59bf1db935f1015514e9391134fc8b467f7f2916
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-04-16 13:37:06 -0400

    deleting desmoines files. Probably much more elegant way to do this, but I'm just a poor caveman programmer and these fancy rebase commands frighten and confuse me

--- ocaml/NetCore.mli
+++
@@ -1,9 +0,0 @@
-(** The NetCore policy language *)
-open OpenFlow0x01Types
-open Packet
-open Platform
-
-module Make : functor (Platform : PLATFORM) -> sig
-  val start_controller : NetCoreSyntax.policy Lwt_stream.t -> unit Lwt.t
-end
-
